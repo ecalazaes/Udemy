efa6c88781c3a473b520a6b0b8c2a5b45bcb4f87
Erick Calazães <ecalazaes@gmail.com>
2024-04-03 03:11:11 -0300

    Solução Muito ruim

--- 01. Java - POO - Nelio Alves/Seção 15 - Tratamento de Exceções/curso_programacao/src/application/TesteExcecoes.java
+++ 01. Java - POO - Nelio Alves/Seção 15 - Tratamento de Exceções/curso_programacao/src/application/TesteExcecoes.java
@@ -4,7 +4,6 @@ import model.entities.Reservation;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.Scanner;
 
 public class TesteExcecoes {
