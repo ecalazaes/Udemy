c1c73f5ac8eb0c1fa9fbdb288e08bc8fc91e614a
Erick Calazães <ecalazaes@gmail.com>
2024-04-12 19:52:17 -0300

    Correção no Programa principal

--- 01. Java - POO - Nelio Alves/Seção 21 - ProjetoDao/ProjetoDAO/src/application/Program.java
+++ 01. Java - POO - Nelio Alves/Seção 21 - ProjetoDao/ProjetoDAO/src/application/Program.java
@@ -5,14 +5,13 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -33,10 +32,10 @@ public class Program {
             System.out.println(obj);
         }
 
-//        System.out.println("\n=== TEST 4: seller Insert ===");
-//        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
-//        sellerDao.insert(newSeller);
-//        System.out.println("Inserted! New id = " + newSeller.getId());
+        System.out.println("\n=== TEST 4: seller Insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
 
         System.out.println("\n=== TEST 5: seller Update ===");
         seller = sellerDao.findById(1);
