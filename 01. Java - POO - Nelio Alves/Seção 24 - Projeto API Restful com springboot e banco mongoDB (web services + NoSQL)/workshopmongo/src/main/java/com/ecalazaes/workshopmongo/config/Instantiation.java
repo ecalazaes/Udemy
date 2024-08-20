34ec7962fe38405d71401ea7791facadd2b6c7e7
Erick Calazães <ecalazaes@gmail.com>
2024-08-20 15:44:19 -0300

    Projeção dos dados do autor com DTO

--- 01. Java - POO - Nelio Alves/Seção 24 - Projeto API Restful com springboot e banco mongoDB (web services + NoSQL)/workshopmongo/src/main/java/com/ecalazaes/workshopmongo/config/Instantiation.java
+++ 01. Java - POO - Nelio Alves/Seção 24 - Projeto API Restful com springboot e banco mongoDB (web services + NoSQL)/workshopmongo/src/main/java/com/ecalazaes/workshopmongo/config/Instantiation.java
@@ -1,5 +1,6 @@
 package com.ecalazaes.workshopmongo.config;
 
+import com.ecalazaes.workshopmongo.dto.AuthorDTO;
 import com.ecalazaes.workshopmongo.entities.Post;
 import com.ecalazaes.workshopmongo.entities.User;
 import com.ecalazaes.workshopmongo.repositories.PostRepository;
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null,sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2= new Post(null,sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null,sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2= new Post(null,sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
