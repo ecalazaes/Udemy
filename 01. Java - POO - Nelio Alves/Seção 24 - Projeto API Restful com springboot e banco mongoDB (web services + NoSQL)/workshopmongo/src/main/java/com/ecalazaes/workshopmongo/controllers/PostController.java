78a1e7bdf11bdcfe90cd24b115de6b8a650e0767
Erick Calazães <ecalazaes@gmail.com>
2024-08-22 14:50:58 -0300

    Consulta com vários critérios

--- 01. Java - POO - Nelio Alves/Seção 24 - Projeto API Restful com springboot e banco mongoDB (web services + NoSQL)/workshopmongo/src/main/java/com/ecalazaes/workshopmongo/controllers/PostController.java
+++ 01. Java - POO - Nelio Alves/Seção 24 - Projeto API Restful com springboot e banco mongoDB (web services + NoSQL)/workshopmongo/src/main/java/com/ecalazaes/workshopmongo/controllers/PostController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -33,6 +34,19 @@ public class PostController {
     public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
         text = URL.decodeParam(text);
         List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping( "/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate)
+    {
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date(0L));
+        List<Post> list = postService.fullSearch(text, min, max);
         return ResponseEntity.ok(list);
     }
 
--- 01. Java - POO - Nelio Alves/Seção 24 - Projeto API Restful com springboot e banco mongoDB (web services + NoSQL)/workshopmongo/src/main/java/com/ecalazaes/workshopmongo/services/PostService.java
+++ 01. Java - POO - Nelio Alves/Seção 24 - Projeto API Restful com springboot e banco mongoDB (web services + NoSQL)/workshopmongo/src/main/java/com/ecalazaes/workshopmongo/services/PostService.java
@@ -5,6 +5,7 @@ import com.ecalazaes.workshopmongo.repositories.PostRepository;
 import com.ecalazaes.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -23,6 +24,11 @@ public class PostService {
 
     public List<Post> findByTitle(String text) {
        return postRepository.findByTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return postRepository.fullSearch(text, minDate, maxDate);
     }
 
 }
