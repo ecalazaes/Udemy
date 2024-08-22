d6151650af79e8bec514781515856fd795ed2fe0
Erick Calazães <ecalazaes@gmail.com>
2024-08-22 14:20:28 -0300

    Consulta simples com @Query

--- 01. Java - POO - Nelio Alves/Seção 24 - Projeto API Restful com springboot e banco mongoDB (web services + NoSQL)/workshopmongo/src/main/java/com/ecalazaes/workshopmongo/repositories/PostRepository.java
+++ 01. Java - POO - Nelio Alves/Seção 24 - Projeto API Restful com springboot e banco mongoDB (web services + NoSQL)/workshopmongo/src/main/java/com/ecalazaes/workshopmongo/repositories/PostRepository.java
@@ -1,14 +1,17 @@
 package com.ecalazaes.workshopmongo.repositories;
 
 import com.ecalazaes.workshopmongo.entities.Post;
-import com.ecalazaes.workshopmongo.entities.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> findByTitle(String title);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
